4336c576150061a8b9309f92fefd106e6fa99383
Ludwig Wiedemann <ludwigwwiedemann@gmail.com>
2021-05-10 12:24:22 +0200

    Add data binding to the app to not use findViewById() all the time

--- AboutMe/app/src/main/java/com/bmwgroup/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/bmwgroup/aboutme/MainActivity.kt
@@ -9,26 +9,35 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.bmwgroup.aboutme.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val buttonDone : Button = findViewById(R.id.done_button)
-        buttonDone.setOnClickListener {
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val buttonDone : Button = findViewById(R.id.done_button)
+//        buttonDone.setOnClickListener {
+//            addNickname(it)
+//        }
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText : EditText = findViewById(R.id.nickname_edit)
-        val nicknameTextView : TextView = findViewById(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
