a5db1a10bae838de34f1b977e19d511f5bc426c9
Ludwig Wiedemann <ludwigwwiedemann@gmail.com>
2021-05-10 12:06:47 +0200

    about me app accidently on master

--- AboutMe/app/src/main/java/com/bmwgroup/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/bmwgroup/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.bmwgroup.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonDone : Button = findViewById(R.id.done_button)
+        buttonDone.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText : EditText = findViewById(R.id.nickname_edit)
+        val nicknameTextView : TextView = findViewById(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
